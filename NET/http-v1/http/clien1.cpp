81aa8aee397e0fd5c5bd7f307d09beb6e3f29a3c
yolo <77111763+yoloeww@users.noreply.github.com>
2023-11-03 09:25:28 +0800

    Update and rename test.c to clien1.cpp

---
+++ NET/http-v1/http/clien1.cpp
@@ -0,0 +1,18 @@
+/*长连接测试1：创建一个客户端持续给服务器发送数据，直到超过超时时间看看是否正常*/
+#include "../source/server.hpp"
+
+int main()
+{
+    Socket cli_sock;
+    cli_sock.CreateClient(8085, "127.0.0.1");
+    std::string req = "GET /hello HTTP/1.1\r\nConnection: keep-alive\r\nContent-Length: 0\r\n\r\n";
+    while(1) {
+        assert(cli_sock.Send(req.c_str(), req.size()) != -1);
+        char buf[1024] = {0};
+        assert(cli_sock.Recv(buf, 1023));
+        DBG_LOG("[%s]", buf);
+        sleep(3);
+    }
+    cli_sock.Close();
+    return 0;
+}
