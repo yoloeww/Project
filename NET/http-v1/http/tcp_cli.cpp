a88f25feb9fb4fd3892aa9e562869d3264408ed5
yolo <77111763+yoloeww@users.noreply.github.com>
2023-11-03 09:29:13 +0800

    Create tcp_cli.cpp

---
+++ NET/http-v1/http/tcp_cli.cpp
@@ -0,0 +1,17 @@
+#include "../source/server.hpp"
+
+int main()
+{
+    Socket cli_sock;
+    cli_sock.CreateClient(8500, "127.0.0.1");
+    for (int i = 0; i < 5; i++) {
+        std::string str = "hello bitejiuyeke!";
+        cli_sock.Send(str.c_str(), str.size());
+        char buf[1024] = {0};
+        cli_sock.Recv(buf, 1023);
+        DBG_LOG("%s", buf);
+        sleep(1);
+    }
+    while(1) sleep(1);
+    return 0;
+}
